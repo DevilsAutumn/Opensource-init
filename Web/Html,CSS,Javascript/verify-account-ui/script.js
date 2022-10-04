544095161f7b0a626dc25e4f41551c6ce21bfee4
kunal00000 <kunalverma2468@gmail.com>
2022-10-04 16:22:05 +0530

    Fixes made

--- Web/Html,CSS,Javascript/verify-account-ui/script.js
+++ Web/Html,CSS,Javascript/verify-account-ui/script.js
@@ -3,12 +3,16 @@ const codes = document.querySelectorAll('.code')
 codes[0].focus()
 
 codes.forEach((code, idx) => {
-    code.addEventListener('keydown', (e) => {
-        if(e.key >= 0 && e.key <=9) {
-            codes[idx].value = ''
+    code.addEventListener('keyup', (e) => {
+        if(e.keyCode >= 48 && e.keyCode <= 57) {
             setTimeout(() => codes[idx + 1].focus(), 10)
         } else if(e.key === 'Backspace') {
+            codes[idx].value = ''
             setTimeout(() => codes[idx - 1].focus(), 10)
+        } else if(e.key === 'Enter'){} 
+        else{
+            codes[idx].value = ''
+            e.preventDefault()
         }
     })
 })
