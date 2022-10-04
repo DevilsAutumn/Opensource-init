71b8fad794339110ad7b24d4a418486ef09e07fe
dhruv18ranjan <97687317+dhruv18ranjan@users.noreply.github.com>
2022-10-05 02:11:36 +0530

    Update App.js

--- Web/React/Greeting-website/src/App.js
+++ Web/React/Greeting-website/src/App.js
@@ -4,8 +4,24 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
+
+let style={};
+
+let hours = new Date().getHours();
+  if (hours >= 0 && hours < 12) {
+    style.backgroundColor = "#87CEEB";
+  } else if (hours >= 12 && hours < 16) {
+    style.backgroundColor = "#FFAE42";
+  } else if (hours >= 16 && hours < 21) {
+    style.backgroundColor = "#007FFF";
+  } else {
+    style.backgroundColor = "#FF2400";
+
+  }
+
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid" style={style}>
       <div className="container">
         <Header />
         <Footer />
