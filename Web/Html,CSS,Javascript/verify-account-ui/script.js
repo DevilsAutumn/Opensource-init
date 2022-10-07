25c2fffcd685c299c2964519fa557611036b8759
kunal00000 <kunalverma2468@gmail.com>
2022-10-07 17:20:56 +0530

    fixes

--- Web/Html,CSS,Javascript/verify-account-ui/script.js
+++ Web/Html,CSS,Javascript/verify-account-ui/script.js
@@ -3,16 +3,21 @@ const codes = document.querySelectorAll('.code')
 codes[0].focus()
 
 codes.forEach((code, idx) => {
-    code.addEventListener('keyup', (e) => {
-        if(e.keyCode >= 48 && e.keyCode <= 57) {
-            setTimeout(() => codes[idx + 1].focus(), 10)
-        } else if(e.key === 'Backspace') {
-            codes[idx].value = ''
-            setTimeout(() => codes[idx - 1].focus(), 10)
-        } else if(e.key === 'Enter'){} 
-        else{
-            codes[idx].value = ''
-            e.preventDefault()
-        }
-    })
-})
+  code.addEventListener("keydown", (e) => {
+    if (e.keyCode >= 48 && e.keyCode <= 57 && codes[idx].value == "") {
+      setTimeout(() => codes[idx + 1].focus(), 10);
+    } else if (e.key === "Backspace") {
+      codes[idx - 1].value = "";
+      setTimeout(() => codes[idx - 1].focus(), 10);
+    } else if (e.key === "Enter") {
+      for (var i = 0; i < codes.length; i++) {
+        codes[i].value = "";
+      }
+      codes[0].focus();
+    } else {
+      codes[idx].value = "";
+      codes[idx].value = e.key;
+      e.preventDefault();
+    }
+  });
+});
